c729382d4480661a772821a30beab617bcf9f41c
kipkoech88 <hoseakipkoech88@gmail.com>
2024-04-20 15:42:35 -0700

    delete a list element

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -3,6 +3,7 @@
 def delete_at(my_list=[], idx=0):
     if idx < 1 or idx > len(my_list):
         return my_list
-    new_list = my_list.copy()
-    del new_list[idx]
-    return new_list
+    else:
+        new_list = my_list.copy()
+        del new_list[idx]
+        return new_list
