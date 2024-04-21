c0d0e439017614aaf8d542569ac0821db1d7290a
kipkoech88 <hoseakipkoech88@gmail.com>
2024-04-21 15:21:49 -0700

    soved 2

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+def uniq_add(my_list=[]):
+    unique = set(my_list)
+    unique_list = list(unique)
+    total = 0
+    for i in range(0, len(unique_list)):
+        total += unique_list[i]
+
+    return total
