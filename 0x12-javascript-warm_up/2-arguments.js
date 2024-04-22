25cbed0cfde3fa397c74977a92b432f48c546b6f
kipkoech88 <hoseakipkoech88@gmail.com>
2024-04-22 06:43:14 -0700

    print arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const { argv } = require('node:process');
+
+if (argv.length > 2) {
+  for (let i = 2; i < argv.length; i++) {
+    console.log(argv[i]);
+  }
+} else {
+  console.log('No argument');
+}
