c86d7e5f48935cf66ccc0b28189088e26abcd959
kipkoech88 <hoseakipkoech88@gmail.com>
2024-04-22 14:03:52 -0700

    safe zerdivision

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    try:
+        c = a / b
+        print('Inside result: {}'.format(c))
+        return c
+    except ZeroDivisionError:
+        print('Inside result: {}'.format(None))
+        return None
