18ad9490cba625e5d218e6b586eb33a3a49211c0
kipkoech88 <hoseakipkoech88@gmail.com>
2024-04-23 16:07:50 -0700

    solved 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+''' class for Square object '''
+
+
+class Square:
+    ''' This is a class that defines a square '''
+
+    def __init__(self, size=0):
+        ''' The object is initialized with 1 attr
+        size which default value is 0
+        and supposed to work even when the programmer
+        forgets to initialize with a value'''
+        self.__size = size
+
+        ''' the size must be an integer otherwise TypeError is
+        raised'''
+        if (type(self.__size) is not int):
+            raise TypeError('size must be an integer')
+        elif (self.__size < 0):
+            raise ValueError('size must be >= 0')
+
+    def area(self):
+        ''' return the square area '''
+        return self.__size * self.__size
