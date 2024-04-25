5b05c4ecf45c0c1dfb8bb3a5a57468efb3841f4f
kipkoech88 <hoseakipkoech88@gmail.com>
2024-04-25 15:19:58 -0700

    add multiple ebtries

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,8 +1,4 @@
 -- create table second_table in the database
--- which has id, name and score
--- if the table exist, the script should not fail
--- the script should add few records
-
 CREATE TABLE IF NOT EXISTS `second_table` (`id` INT, `name` VARCHAR(256), `score` INT);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Alex", 3);
