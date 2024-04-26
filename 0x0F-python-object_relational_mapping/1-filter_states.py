e16348f28f54966eecece82a857aa9f1ba5088fb
kipkoech88 <hoseakipkoech88@gmail.com>
2024-04-26 12:55:35 -0700

    initial commit

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+import sys
+import MySQLdb
+""" This module displays values in
+the `states` table of hbtn_0e_0_usa
+where name matches the argument
+"""
+
+if __name__ == "__main__":
+    """ Takes 4 command line arguments ie username
+    password, db name and name to be searched for
+    """
+    db = MySQLdb.connect(host="localhost",
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         port=3306)
+    cur = db.cursor()
+    cur.execute("""SELECT * FROM states WHERE name
+                LIKE BINARY 'N%'
+                ORDER BY states.id ASC""")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
