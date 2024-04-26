d74ae084b3c2460c93b70db8db427c96bb383485
kipkoech88 <hoseakipkoech88@gmail.com>
2024-04-26 13:13:46 -0700

    filter

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -16,9 +16,8 @@ if __name__ == "__main__":
                          db=sys.argv[3],
                          port=3306)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name
-                LIKE BINARY {}
-                ORDER BY states.id ASC".format(sys.argv[4]))
+    cur.execute("""SELECT * FROM states WHERE name 
+                LIKE BINARY '{}' ORDER BY states.id ASC""".format(sys.argv[4]))
     rows = cur.fetchall()
     for row in rows:
         print(row)
