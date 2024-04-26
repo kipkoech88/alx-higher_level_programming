b986080385821e753fb31c31689478aca9b16f48
kipkoech88 <hoseakipkoech88@gmail.com>
2024-04-26 13:16:05 -0700

    filter

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,23 +1,15 @@
 #!/usr/bin/python3
-import sys
+"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
-""" This module displays values in
-the `states` table of hbtn_0e_0_usa
-where name matches the argument
-"""
+import sys
+
 
 if __name__ == "__main__":
-    """ Takes 4 command line arguments ie username
-    password, db name and name to be searched for
-    """
-    db = MySQLdb.connect(host="localhost",
-                         user=sys.argv[1],
-                         passwd=sys.argv[2],
-                         db=sys.argv[3],
-                         port=3306)
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
-    cur.execute("""SELECT * FROM states WHERE name 
-                LIKE BINARY '{}' ORDER BY states.id ASC""".format(sys.argv[4]))
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
+                .format(sys.argv[4]))
     rows = cur.fetchall()
     for row in rows:
         print(row)
