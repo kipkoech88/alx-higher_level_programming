f1f8d379c1aaef9cbd3feb0ce842114e2cb4964d
kipkoech88 <hoseakipkoech88@gmail.com>
2024-05-09 13:10:11 -0700

    create rectangle

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+""" Create an  Rectangle object """
+
+
+class Rectangle():
+    """
+    create an Rectangleo object
+    with width and height
+    """
+    def __init__(self, width=0, height=0):
+        """ initialize the Rectangle obj"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """ get the width value """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ set the wisth value """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif (value < 0):
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """ get the height value"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ set the height value """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif (value < 0):
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
