46149f3ae7596897f9d6fef261f6f6e47b2c8952
kipkoech88 <hoseakipkoech88@gmail.com>
2024-05-09 07:15:29 -0700

    post to the server

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # post data to server via URL
-curl -s -X POST "email=test@gmail.com&subject=I will always be here for PLD" "$1"
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
