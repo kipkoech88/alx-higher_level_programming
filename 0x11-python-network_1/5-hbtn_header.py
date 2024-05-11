38dc425262e064ee2e64bbe8684e7cf32fe64d7a
kipkoech88 <hoseakipkoech88@gmail.com>
2024-05-11 03:44:01 -0700

    display value of header var

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" take in URL as arg
+and display value of
+variable in response
+headers """
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    r = requests.get(url)
+    print(r.headers['X-Request-Id'])
