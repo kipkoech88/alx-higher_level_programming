5576181724b8fc70bcfccf95d2ebd15e4b251b24
kipkoech88 <hoseakipkoech88@gmail.com>
2024-05-11 04:01:15 -0700

    post data

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -9,6 +9,6 @@ import requests
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    vale = {"email": sys.argv[2]}
+    value = {"email": sys.argv[2]}
     r = requests.post(url, data=value)
     print(r.text)
