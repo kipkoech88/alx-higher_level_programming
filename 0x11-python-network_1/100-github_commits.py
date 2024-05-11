b8a998d6344e12a1c653ab96a08884b04e71fb3b
kipkoech88 <hoseakipkoech88@gmail.com>
2024-05-11 05:31:59 -0700

    check commits

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -14,5 +14,5 @@ if __name__ == "__main__":
     r = requests.get(url)
     commits = r.json()
     for i in range(10):
-        print("{} {}".format(commits[i].get("sha"),
+        print("{}: {}".format(commits[i].get("sha"),
               commits[i].get("commit").get("author").get("name")))
