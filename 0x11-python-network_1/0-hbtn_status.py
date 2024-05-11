be4eed451a422c0f13bab2e5ec4a2d23d816fd6e
kipkoech88 <hoseakipkoech88@gmail.com>
2024-05-11 02:28:44 -0700

    initial commit

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+#  fetches https://alx-intranet.hbtn.io/status
+import urllib.request
+
+if __name__ == "__main__":
+    req = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(req) as response:
+        html = response.read()
+        print("Body response:")
+        print("\t - type: {}".format(type(html)))
+        print("\t - content: {}".format(html))
+        print("\t - utf8 content: {}".format(html.decode("utf8")))
