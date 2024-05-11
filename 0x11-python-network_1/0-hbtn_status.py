c952c0b4a90b5325a8690ef0d2ae0a2ec2f9a218
kipkoech88 <hoseakipkoech88@gmail.com>
2024-05-11 02:37:07 -0700

    initial commit

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-#  fetches https://alx-intranet.hbtn.io/status
+"""  fetches https://alx-intranet.hbtn.io/status"""
 import urllib.request
 
 if __name__ == "__main__":
     req = urllib.request.Request("https://alx-intranet.hbtn.io/status")
     with urllib.request.urlopen(req) as response:
         html = response.read()
-        print("Body response:$")
-        print("\t - type: {}$".format(type(html)))
-        print("\t - content: {}$".format(html))
-        print("\t - utf8 content: {}$".format(html.decode("utf8")))
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode("utf8")))
